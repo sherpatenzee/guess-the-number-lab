2af78087f0e5860bbe8ace122744e50fc2c59fe4
tenzing sherpa <sherpatenzee09@gmail.com>
2022-01-31 17:27:23 -0500

    complete guess the number lab

--- app.js
+++ app.js
@@ -3,8 +3,48 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+     console.log(this.secretNum)
+      do{
+       this.prevGuesses.push(this.getGuess())
+      
+       this.render()
+       
+      }while(this.userInput!== this.secretNum)
+      return 
+     
+    }
+
+}
+
+game.prevGuesses= []
+
+game.getGuess=function(){
+this.userInput=prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
+
+this.userInput= parseInt(this.userInput)
+while(isNaN(this.userInput)=== true || this.userInput > this.biggestNum || this.userInput<this.smallestNum){
+  this.userInput=prompt(`Reenter a guess between ${this.smallestNum} and ${this.biggestNum}.`) 
+  this.userInput= parseInt(this.userInput)}
+return this.userInput
+
+}
+
+
+game.render=function(){
+if(this.userInput===this.secretNum){
+alert(`Congrats! You guessed the number in ${this.prevGuesses.length}!`)
+} else {
+  if(this.userInput<this.secretNum){
+  alert(`Your Guess is too low. Previous guesses:${this.prevGuesses.join()} `)
+  } else {
+    alert(`Your Guess is too high. Previous guesses:${this.prevGuesses.join()}`)
   }
 }
+
+}
+console.log(game.play())
+
